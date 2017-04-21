bfa56baf10af08b1cf7835e502a77979757c0561
Oleksandr Bodrov <bodrovphone@gmail.com>
2017-04-21 16:12:55 +0300

    lesson 10 done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,63 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import { createStore } from 'redux';
+// import App from './App';
+// import './index.css';
 
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/css/bootstrap-theme.css';
+// import 'bootstrap/dist/css/bootstrap.css';
+// import 'bootstrap/dist/css/bootstrap-theme.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <App />,
+//   document.getElementById('root')
+// );
+
+
+const counter = (state = 0, action) => {
+    switch (action.type) {
+        case 'INCREMENT':
+            return state + 1;
+        case 'DECREMENT':
+            return state - 1;
+        default:
+            return state;
+    }
+}
+
+
+const store = createStore(counter);
+
+const Counter = ({ 
+    value,
+    onIncrement,
+    onDecrement
+     }) => (
+        <div>
+            <h1>{value}</h1>
+            <button onClick={onIncrement}>+</button>
+            <button onClick={onDecrement}>-</button>
+        </div>
+    );
+
+
+const render = () => {
+    ReactDOM.render(
+        <Counter 
+            value={store.getState()} 
+            onIncrement={() => 
+                store.dispatch({
+                    type: 'INCREMENT'
+                })
+            }
+            onDecrement={() => 
+                store.dispatch({
+                    type: 'DECREMENT'
+                })
+            }
+        />,
+        document.getElementById('root')
+        );
+};
+
+store.subscribe(render);
+render();
