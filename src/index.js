9c170409e3f6495044c25d0d8147a26a24d50a25
Oleksandr Bodrov <bodrovphone@gmail.com>
2017-04-24 18:14:29 +0300

    lesson 11

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
+import expect from 'expect';
+import deepFreeze from 'deep-freeze-strict'
 // import App from './App';
 // import './index.css';
 
@@ -13,51 +15,92 @@ import { createStore } from 'redux';
 // );
 
 
-const counter = (state = 0, action) => {
-    switch (action.type) {
-        case 'INCREMENT':
-            return state + 1;
-        case 'DECREMENT':
-            return state - 1;
+// const counter = (state = 0, action) => {
+//     switch (action.type) {
+//         case 'INCREMENT':
+//             return state + 1;
+//         case 'DECREMENT':
+//             return state - 1;
+//         default:
+//             return state;
+//     }
+// }
+
+
+// const store = createStore(counter);
+
+// const Counter = ({ 
+//     value,
+//     onIncrement,
+//     onDecrement
+//      }) => (
+//         <div>
+//             <h1>{value}</h1>
+//             <button onClick={onIncrement}>+</button>
+//             <button onClick={onDecrement}>-</button>
+//         </div>
+//     );
+
+
+// const render = () => {
+//     ReactDOM.render(
+//         <Counter 
+//             value={store.getState()} 
+//             onIncrement={() => 
+//                 store.dispatch({
+//                     type: 'INCREMENT'
+//                 })
+//             }
+//             onDecrement={() => 
+//                 store.dispatch({
+//                     type: 'DECREMENT'
+//                 })
+//             }
+//         />,
+//         document.getElementById('root')
+//         );
+// };
+
+// store.subscribe(render);
+// render();
+const todos = (state = [], action) => {
+    switch(action.type) {
+        case 'ADD_TODO':
+            return [
+                ...state,
+                {
+                    id: action.id,
+                    text: action.text,
+                    completed: false
+                }
+            ];
         default:
             return state;
     }
-}
-
-
-const store = createStore(counter);
-
-const Counter = ({ 
-    value,
-    onIncrement,
-    onDecrement
-     }) => (
-        <div>
-            <h1>{value}</h1>
-            <button onClick={onIncrement}>+</button>
-            <button onClick={onDecrement}>-</button>
-        </div>
-    );
-
-
-const render = () => {
-    ReactDOM.render(
-        <Counter 
-            value={store.getState()} 
-            onIncrement={() => 
-                store.dispatch({
-                    type: 'INCREMENT'
-                })
-            }
-            onDecrement={() => 
-                store.dispatch({
-                    type: 'DECREMENT'
-                })
-            }
-        />,
-        document.getElementById('root')
-        );
 };
 
-store.subscribe(render);
-render();
+const testAddTodo = () => {
+    const stateBefore = [];
+    const action = {
+        type: 'ADD_TODO',
+        id: 0,
+        text: 'Learn Redux'
+    };
+    const stateAfter = [
+        {
+            id: 0,
+            text: 'Learn Redux',
+            completed: false
+        }
+    ];
+
+    deepFreeze(stateBefore);
+    deepFreeze(stateAfter);
+
+    expect(
+        todos(stateBefore, action)
+        ).toEqual(stateAfter);
+};
+
+testAddTodo();
+console.log('All tests passed.');
